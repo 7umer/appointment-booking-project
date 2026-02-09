9def37e36d0c4f083c58fdeb34e819fad3bdea37
7umer <umerbadal@gmail.com>
2026-02-09 19:59:08 +0530

    create admin after migrate

--- Appointment_Booking_API/api/apps.py
+++ Appointment_Booking_API/api/apps.py
@@ -6,12 +6,4 @@ class ApiConfig(AppConfig):
     name = 'api'
 
     def ready(self):
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        if not User.objects.filter(email="admin@gmail.com").exists():
-            User.objects.create_superuser(
-                email="admin@gmail.com",
-                password="admin123"
-            )
+        import api.signals
