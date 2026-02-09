f60966498bf85cbc4c6362a05ff4f257f7ac599a
7umer <umerbadal@gmail.com>
2026-02-09 19:50:29 +0530

    auto create render admin

--- Appointment_Booking_API/api/apps.py
+++ Appointment_Booking_API/api/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if not User.objects.filter(email="admin@gmail.com").exists():
+            User.objects.create_superuser(
+                email="admin@gmail.com",
+                password="admin123"
+            )
