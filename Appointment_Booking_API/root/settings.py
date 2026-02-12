a9127276fca78cbba8fafc1dbcca80589c92f7c8
7umer <umerbadal@gmail.com>
2026-02-12 11:50:35 +0530

    use postgres only

--- Appointment_Booking_API/root/settings.py
+++ Appointment_Booking_API/root/settings.py
@@ -79,17 +79,12 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # DATABASE
 # -----------------------
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
 
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600, ssl_require=False)
+    "default": dj_database_url.config(conn_max_age=600)
 }
 
 
