4b12faa2def55c7a7a97c153f5c3ad351da27406
7umer <umerbadal@gmail.com>
2026-02-12 11:37:11 +0530

    final postgres admin

---
+++ Appointment_Booking_API/create_admin.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(email="admin@gmail.com").exists():
+    User.objects.create_superuser(
+        email="admin@gmail.com",
+        password="admin123"
+    )
+    print("Admin created")
+else:
+    print("Admin already exists")
--- Appointment_Booking_API/root/settings.py
+++ Appointment_Booking_API/root/settings.py
@@ -79,13 +79,18 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # DATABASE
 # -----------------------
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=False)
 }
-
-
 
 
 
